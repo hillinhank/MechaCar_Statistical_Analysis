e87e0e29ae35c5df9432abe4b200433443d94d84
hillinhank <101481759+hillinhank@users.noreply.github.com>
2022-06-18 14:39:11 -0500

    Deliverable 2 lot_summary

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -1,13 +1,19 @@
 library(dplyr)
 carmpgdf <- read.csv(file = "MechaCar_mpg.csv")
 head(carmpgdf)
-lin1 = lm(formula = vehicle_length~mpg + vehicle_weight + spoiler_angle + ground_clearance + AWD, 
+lin1 = lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, 
                      data=carmpgdf)
 summary(lin1)$r.squared
-summary(aov(vehicle_length~mpg + vehicle_weight + spoiler_angle + ground_clearance + AWD, 
+summary(aov(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, 
             data=carmpgdf))
 
+lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, 
+   data=carmpgdf)
 
+suspdf <- read.csv(file = "Suspension_Coil.csv")
+head(suspdf)
+total_summary <- summarise(suspdf, avg.PSI=mean(PSI), median.PSI=median(PSI), var.PSI=var(PSI), standard.dev.PSI=sd(PSI))
 
-
-
+lot_summary <- suspdf %>% group_by(Manufacturing_Lot) %>% summarise(total_summary, avg.PSI=mean(PSI), median.PSI=median(PSI), var.PSI=var(PSI), standard.dev.PSI=sd(PSI), .groups = 'drop')
+head(lot_summaryy)
+                summarize(math_hope, avg.math=mean(math_score), avg.reading=mean(reading_score))
